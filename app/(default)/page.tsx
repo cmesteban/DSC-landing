e80f4fdd556f4ea0885e3e239aeaa3b66b709afa
Carlos Esteban <estebcar29@gmail.com>
2023-08-02 22:04:13 -0500

    made some quick updates

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: 'Home - Open PRO',
+  title: 'Home - Do Stuff Club',
   description: 'Page description',
 }
 
--- components/features.tsx
+++ components/features.tsx
@@ -6,7 +6,7 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2>
+            <h2 className="h2 mb-4">Our mission is to inspire the doing of more things with more people</h2>
             <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
 
